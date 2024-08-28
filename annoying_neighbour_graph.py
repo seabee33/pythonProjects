674b381a6f2fb33b574dc53ae90e61a3f931f765
SeaBee <85791528+seabee33@users.noreply.github.com>
2024-08-28 16:18:30 +1000

    Update annoying_neighbour_graph.py

--- annoying_neighbour_graph.py
+++ annoying_neighbour_graph.py
@@ -5,45 +5,24 @@
 import mysql.connector, matplotlib.pyplot as plt, datetime
 from collections import Counter
 from matplotlib.ticker import MaxNLocator
-from matplotlib.animation import FuncAnimation
 
-conn = mysql.connector.connect(host="localhost", database="seabee", user="readonly", password="readonly")
+conn = mysql.connector.connect(host="192.168.4.222", database="seabee", user="readonly", password="readonly")
 
 with conn.cursor() as cursor:
     cursor.execute("SELECT time FROM dog ORDER BY time ASC") 
     times_r = cursor.fetchall()
-    times = [datetime.datetime.strftime(time[0], "%H:%M") for time in times_r] #data is a list of times HH:MM format
+    times = [datetime.datetime.strftime(time[0], "%H:%M") for time in times_r] # Data is a list of times in HH:MM format
     time_counts = Counter(times)
 
     times_sorted = sorted(time_counts.keys())
     counts = [time_counts[time] for time in times_sorted]
 
-    # Get min and max times
-    min_time = min(times)
-    max_time = max(times)
+    plt.figure(figsize=(10,6))
+    plt.bar(times_sorted, counts, color='blue', edgecolor='black')
+    plt.xticks(rotation=45)
 
-    # Format min and max time
-    min_time_formatted = datetime.datetime.strptime(min_time, "%H:%M")
-    max_time_formatted = datetime.datetime.strptime(max_time, "%H:%M")
-    full_time_range = []
-
-    current_time = min_time_formatted
-    while current_time <= max_time_formatted:
-        full_time_range.append(current_time.strftime("%H:%M"))
-        current_time += datetime.timedelta(minutes=1)
-
-    counts = [time_counts.get(time, 0) for time in full_time_range]
-
-    # Generate the chart
-    plt.figure(figsize=(14,8))
-    plt.bar(full_time_range, counts, color='blue', edgecolor='black')
-    plt.xticks(rotation=45, fontsize=9)
-
-    plt.gca().yaxis.set_major_locator(MaxNLocator(integer=True))
-
-    plt.xlabel("Times")
-    plt.ylabel("Frequency")
-    plt.title("Time annoying neighbour annoys me every morning")
+    plt.gca().yaxis.set_major_locator(MaxNLocator(integer=True))  # Fixed typo here
 
     plt.tight_layout()
     plt.show()
+
